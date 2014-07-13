f02b7cf1f9a44b8e0f8bb2eea0e874b0f5c28ce2
RahulSwami01 <million.rahul@gmail.com>
2014-07-13 07:54:04 +0530

    Update Plot4.R

--- Plot4.R
+++ Plot4.R
@@ -1,7 +1,3 @@
-data<-read.table("household_power_consumption.txt", sep=";", header =T, 
-                 colClasses=c('character', 'character', 'numeric', 'numeric', 'numeric', 'numeric','numeric', 'numeric', 'numeric'),
-                 na.strings='?')
-# Extract data for 2 dates: Feb 01,2007 and Feb 02, 2007
 subdata<-data[data$Date %in% c("1/2/2007","2/2/2007"),]                 
 
 # Get the date
@@ -9,3 +5,11 @@ subdata$datetime<-strptime(paste(subdata$Date,subdata$Time),"%d/%m/%Y %H:%M:%S")
 
 # Open plot4.png
 png("plot4.png", height = 480, width = 480)
+
+# multiplot
+par(mfrow=c(2,2))
+# Global Active Power plot
+plot(subdata$datetime,subdata$Global_active_power,xlab ="", ylab = "Global Active Power", type ="l")
+
+# Voltage plot
+plot(subdata$datetime,subdata$Voltage,xlab ="datetime", ylab = "Voltage", type ="l")

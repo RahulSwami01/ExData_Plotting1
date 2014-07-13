37ba23ac4c5aa6b87bb0619227980624da5844b1
RahulSwami01 <million.rahul@gmail.com>
2014-07-13 07:54:34 +0530

    Update Plot4.R

--- Plot4.R
+++ Plot4.R
@@ -1,3 +1,10 @@
+##========================================================================================================================
+## plot4.R: Using subplot in R
+# Read data, specify the format of each variable, na.strings is coded as ?, date in format dd/mm/yyyy, time in format hh:mm:ss
+data<-read.table("household_power_consumption.txt", sep=";", header =T, 
+                 colClasses=c('character', 'character', 'numeric', 'numeric', 'numeric', 'numeric','numeric', 'numeric', 'numeric'),
+                 na.strings='?')
+# Extract data for 2 dates: Feb 01,2007 and Feb 02, 2007
 subdata<-data[data$Date %in% c("1/2/2007","2/2/2007"),]                 
 
 # Get the date
@@ -13,3 +20,15 @@ plot(subdata$datetime,subdata$Global_active_power,xlab ="", ylab = "Global Activ
 
 # Voltage plot
 plot(subdata$datetime,subdata$Voltage,xlab ="datetime", ylab = "Voltage", type ="l")
+
+# Energy sub metering
+plot(subdata$datetime,subdata$Sub_metering_1,xlab ="", ylab = "Energy sub metering", type ="l",col = 'black')
+lines(subdata$datetime,subdata$Sub_metering_2, col = "red")
+lines(subdata$datetime,subdata$Sub_metering_3, col = "blue")
+legend('topright',c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c('black','red','blue'), lty = 1, lwd = 3)
+
+# Global Reactive power
+plot(subdata$datetime,subdata$Global_reactive_power,xlab ="datetime", ylab = "Global_reactive_power", type ="l")
+
+# Close png file
+dev.off()

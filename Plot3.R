e349b3341c633a2624512b36429f59c79782c0b4
RahulSwami01 <million.rahul@gmail.com>
2014-07-13 07:52:33 +0530

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -1,3 +1,9 @@
+##========================================================================================================================
+## plot3.R: Energy sub metering Power in two days Feb 01, 2007 and Feb 02, 2007
+## Sub_metering_1: in black
+## Sub_metering_2: in red
+## Sub_metering_3: in blue
+# Read data, specify the format of each variable, na.strings is coded as ?, date in format dd/mm/yyyy, time in format hh:mm:ss
 data<-read.table("household_power_consumption.txt", sep=";", header =T, 
                  colClasses=c('character', 'character', 'numeric', 'numeric', 'numeric', 'numeric','numeric', 'numeric', 'numeric'),
                  na.strings='?')
@@ -12,3 +18,7 @@ png("plot3.png", height = 480, width = 480)
 plot(subdata$datetime,subdata$Sub_metering_1,xlab ="", ylab = "Energy sub metering", type ="l",col = 'black')
 lines(subdata$datetime,subdata$Sub_metering_2, col = "red")
 lines(subdata$datetime,subdata$Sub_metering_3, col = "blue")
+legend('topright',c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c('black','red','blue'), lty = 1, lwd = 3)
+
+# Close png file
+dev.off()

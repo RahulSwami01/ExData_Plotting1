dde32e65be2668255f9cf448863e708124f926d7
RahulSwami01 <million.rahul@gmail.com>
2014-07-13 07:50:46 +0530

    Update Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -1,3 +1,5 @@
+## plot2.R: Global Active Power in two days Feb 01, 2007 and Feb 02, 2007
+# Read data, specify the format of each variable, na.strings is coded as ?, date in format dd/mm/yyyy, time in format hh:mm:ss
 data<-read.table("household_power_consumption.txt", sep=";", header =T, 
                  colClasses=c('character', 'character', 'numeric', 'numeric', 'numeric', 'numeric','numeric', 'numeric', 'numeric'),
                  na.strings='?')
@@ -9,3 +11,6 @@ subdata$datetime<-strptime(paste(subdata$Date,subdata$Time),"%d/%m/%Y %H:%M:%S")
 
 # Open plot2.png
 png("plot2.png", height = 480, width = 480)
+plot(subdata$datetime,subdata$Global_active_power,xlab ="", ylab = "Global Active Power (kilowatts)", type ="l")
+# Close png file
+dev.off()
